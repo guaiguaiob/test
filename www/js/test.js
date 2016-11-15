272c74cf8a536f386778f7ebfd20b61052e10c3f
guaiguaiob <guaiguaiob@outlook.com>
2016-11-15 14:32:07 +0800

    Update test.js

--- www/js/test.js
+++ www/js/test.js
@@ -3,16 +3,19 @@ var pictureSource;
 var destinationType;
 
 function test_init() {
+  console.log('test_init');
   pictureSource=navigator.camera.PictureSourceType;
   destinationType=navigator.camera.DestinationType;
 }
 
 function onPhotoDataSuccess(imageData) {
+  console.log('onPhotoDataSuccess');
   var smallImage = document.getElementById('smallImage');
   smallImage.style.display = 'block';
   smallImage.src = "data:image/jpeg;base64," + imageData;
 }
 
 function capturePhotoWithData() {
+  console.log('capturePhotoWithData');
   navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50 });
 }
