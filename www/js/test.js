05b66ba3e9772876c1bdd6311de113960a4c48fc
guaiguaiob <guaiguaiob@outlook.com>
2016-11-15 14:23:49 +0800

    Update test.js

--- www/js/test.js
+++ www/js/test.js
@@ -1,1 +1,18 @@
 
+var pictureSource;
+var destinationType;
+
+function test_init() {
+  pictureSource=navigator.camera.PictureSourceType;
+  destinationType=navigator.camera.DestinationType;
+}
+
+function onPhotoDataSuccess(imageData) {
+  var smallImage = document.getElementById('smallImage');
+  smallImage.style.display = 'block';
+  smallImage.src = "data:image/jpeg;base64," + imageData;
+}
+
+function capturePhotoWithData() {
+  navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50 });
+}
