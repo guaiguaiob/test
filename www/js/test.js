e3dee2845ba63a17e74cc37f8c0e84caa492ba70
guaiguaiob <guaiguaiob@outlook.com>
2016-11-17 00:21:54 +0800

    Update test.js

--- www/js/test.js
+++ www/js/test.js
@@ -1,63 +1,8 @@
-
-var pictureSource;
-var destinationType;
-
-function test_init() {
-  console.log('test_init');
-  pictureSource=navigator.camera.PictureSourceType;
-  destinationType=navigator.camera.DestinationType;
-}
-
-function onPhotoDataSuccess(imageData) {
-  console.log('onPhotoDataSuccess');
-  var smallImage = document.getElementById('smallImage');
-  smallImage.style.display = 'block';
-  smallImage.src = "data:image/jpeg;base64," + imageData;
-}
-
-function onPhotoFail(message) {
-  alert('Failed because: ' + message);
-}
-
-function capturePhotoWithData() {
-  console.log('capturePhotoWithData');
-  navigator.camera.getPicture(onPhotoDataSuccess, onPhotoFail, { quality: 50 });
-}
-
-function onMonitoringSuccess(regionState) {
-  console.log('State is ' + regionState.state)
-}
-
-function onMonitoringError(message) {
-  alert('Failed because: ' + message);
-}
-
-function startMonitoring() {
-  EstimoteBeacons.startMonitoringForRegion (
-    {}, // Empty region matches all beacons.
-    function(result) {
-      console.log('*** Beacons monitored ***')
-      console.log(result);
-      estimote.beacons.printObject(result) },
-    function(errorMessage) {
-      console.log('Monitoring error: ' + errorMessage) });
-}
-
-function stopMonitoring() {
-  EstimoteBeacons.stopMonitoringForRegion(
-    {},
-     function(result) {
-       console.log('*** Beacons monitoring stopped ***')
-       console.log(result)},
-    function(errorMessage) {
-       console.log('Stop monitoring error: ' + errorMessage) });
-}
-
 function startRanging() {
   iiibeacon.startRanging(
     function(result) {
       console.log('*** Beacons ranged ***')
-      console.log(result);
+      console.log(result) },
     function(errorMessage) {
       console.log('Ranging error: ' + errorMessage) });
 }
